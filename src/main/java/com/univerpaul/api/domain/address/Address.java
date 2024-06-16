362c549398e2d20678058e240db6587d4f2388af
Paulo N. Martins <paulomartinsasd@hotmail.com>
2024-06-16 15:06:58 -0300

    feat: creating entities

---
+++ src/main/java/com/univerpaul/api/domain/address/Address.java
@@ -0,0 +1,21 @@
+package com.univerpaul.api.domain.address;
+
+import com.univerpaul.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
