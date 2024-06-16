f0856ad2479841909a358546a2bd25c0775e4823
Paulo N. Martins <paulomartinsasd@hotmail.com>
2024-06-16 15:12:32 -0300

    feat: add repositories

---
+++ src/main/java/com/univerpaul/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.univerpaul.api.repositories;
+
+import com.univerpaul.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
